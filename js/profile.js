772dfb9873c9bd876fb0e609ee8a99b5474372c7
Subash-Vadivel <88760720+Subash-Vadivel@users.noreply.github.com>
2023-03-17 10:03:44 +0530

    1.2

--- js/profile.js
+++ js/profile.js
@@ -1,13 +1,13 @@
 function logout()
 {
     localStorage.removeItem('uid');
-    window.location.replace("http://127.0.0.1:5500/index.html");
+    window.location.replace("https://guvi05.netlify.app");
 }
 function route()
 {
     if(localStorage.getItem('uid')===undefined || localStorage.getItem('uid')==='')
     {
-        window.location.replace("http://127.0.0.1:5500/index.html");
+        window.location.replace("https://guvi05.netlify.app");
     }
     else
     {
